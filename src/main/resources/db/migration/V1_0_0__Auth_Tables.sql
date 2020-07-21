529d46412431aca3f173d2bd5a10ccfd8ea2adbb
MuslimArsakaev <46483086+MuslimArsakaev@users.noreply.github.com>
2020-07-21 16:55:58 +0600

    updated migrtion file

--- src/main/resources/db/migration/V1_0_0__Auth_Tables.sql
+++ src/main/resources/db/migration/V1_0_0__Auth_Tables.sql
@@ -27,9 +27,9 @@ create table users (
     first_name character varying(100) not null,
     last_name character varying(100) not null,
     password character varying(100) not null,
-    reg_date date default current_date ,
-    upd_date date default current_date ,
-    state character(1) default 'active'::bpchar not null
+    reg_date date default current_date,
+    upd_date date default current_date,
+    state character(20) default 'ACTIVE'::bpchar not null
 );
 
 create table roles (
@@ -37,20 +37,13 @@ create table roles (
     role varchar,
     description varchar,
     created_at date default current_date
-)
+);
 
 create table user_roles (
     user_role_id integer default nextval('user_roles_user_role_id_seq'::regclass) not null,
     user_id integer not null,
-    role_id character varying(20)
+    role_id integer not null
 );
-
-
-alter table only user_roles
-    add constraint fk_user_roles_users foreign key (user_id) references users(user_id);
-
-alter table only user_roles
-    add constraint fk_user_roles_roles foreign key (role_id) references roles(role_id);
 
 alter table only users
     add constraint pkusers primary key (user_id);
@@ -60,6 +53,12 @@ alter table only roles
 
 alter table only user_roles
     add constraint pkuser_roles primary key (user_role_id);
+
+alter table only user_roles
+    add constraint fk_user_roles_users foreign key (user_id) references users(user_id);
+
+alter table only user_roles
+    add constraint fk_user_roles_roles foreign key (role_id) references roles(role_id);
 
 
 insert into users(email, first_name, last_name, username, password)
